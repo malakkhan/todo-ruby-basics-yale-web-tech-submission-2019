e17dd27e232bab9ea8cffab9011d078a8d267b91
enchanting-point-0043 <malak.khan@yale.edu>
2019-02-18 08:02:14 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,15 +3,14 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-
+  name = value
 end
 
-def argue
-
+def argue(argument)
+  return argument
 end
 
-def greeting
-
+def greeting(name, arg2)
 end
 
 def return_a_value
